e07f8cf161456c058bc9d49ae7d59e90d0add2c5
Brian Patrick <batpat772@gmail.com>
2017-09-23 07:49:43 -0700

    last commit, couldn't figure out how to get moment working

--- assets/app.js
+++ assets/app.js
@@ -41,6 +41,8 @@ $("form")[0].reset();
    };
 
 
+//calling fullData function will fill the "Current Train Schedule" be
+
 
 //on click for the "Add a Train" button to store data into the database.
 $("#submit").on("click", function(){
@@ -68,3 +70,41 @@ fillData();
 }); //#submit on click function closer
 /***/
 
+// //Moment js stuff
+// // var randomDate = "09/20/2017";
+// // var randomFormat = "MM/DD/YYYY";
+// // var convertedDate = moment(randomDate, randomFormat);
+// // Using scripts from moment.js write code below to complete each of the following.
+// // Console.log to confirm the code changes you made worked.
+// // 1 ...to convert the randomDate into three other date formats
+// console.log(moment(convertedDate).format("MM/DD/YY"));
+// console.log(moment(convertedDate).format("MMM Do, YYYY hh:mm:ss"));
+// console.log(moment(convertedDate).format("X"));
+// console.log("----------------------------------------");
+// // 2 ...to determine the time in years, months, days between today and the randomDate
+// console.log(moment(convertedDate).toNow());
+// console.log(moment(convertedDate).diff(moment(), "years"));
+// console.log(moment(convertedDate).diff(moment(), "months"));
+// console.log(moment(convertedDate).diff(moment(), "days"));
+// console.log("----------------------------------------");
+// // 3 ...to determine the number of days between the randomDate and 02/14/2001
+// var newDate = moment("02/14/2001", randomFormat);
+// console.log(moment(convertedDate).diff(moment(newDate), "days"));
+// // 4 ...to convert the randomDate to unix time (be sure to look up what unix time even is!!!)
+// console.log(moment(convertedDate).format("X"));
+// console.log("----------------------------------------");
+// // 5 ...to determine what day of the week and what week of the year this randomDate falls on.
+// console.log(moment(convertedDate).format("DDD"));
+// console.log(moment(convertedDate).format("dddd"));
+
+var today = moment();
+today.isBefore('23:30', 'hh:mm');
+var tomorrow = moment();
+
+console.log(today.isBefore('23:30', "hh:mm"));
+
+var hours = 4;
+var minutes = 30;
+console.log(moment("09/21/2017 4:00 AM").format());
+var newTime = moment().add(hours, 'h').add(minutes, 'm').format();
+console.log(newTime);
